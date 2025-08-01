52be93244f6ab154a89abb680e3b3d004e22ac7d
FlickshotFire <136790125+FlickshotFire@users.noreply.github.com>
2025-08-02 02:13:00 +0530

    feat: update t.js

--- t.js
+++ t.js
@@ -1,4 +1,21 @@
-console.log('Aman');
+DOCTYPE
+<html>
+<body>
+
+<h2>JavaScriptExpressions</h2>
+
+<p>Expressions</p>
+
+<p id="demo"></p>
+
+<script>
+"demo""John"" ""Doe"
+</script>
+
+</body>
+</html>
+
+
 
 
 
