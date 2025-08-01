97d5bfe0ba0b966260c2705c73ead48ee671c047
FlickshotFire <136790125+FlickshotFire@users.noreply.github.com>
2025-08-02 02:11:26 +0530

    feat: update n.js

--- n.js
+++ n.js
@@ -1,1 +1,17 @@
-console.log('Aman');
+DOCTYPE
+<html>
+<body>
+
+<h2>JavaScriptExpressions</h2>
+
+<p>Expressions</p>
+
+<p id="demo"></p>
+
+<script>
+"demo""John"" ""Doe"
+</script>
+
+</body>
+</html>
+
