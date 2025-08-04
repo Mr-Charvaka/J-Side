1c7d4c8342becbaa5d806e41298941a195d7dbab
FlickshotFire <136790125+FlickshotFire@users.noreply.github.com>
2025-08-04 22:39:56 +0530

    feat: update Two Sum.js

--- Two Sum.js
+++ Two Sum.js
@@ -1,10 +1,18 @@
-const twoSum = function(nums, target) {
-    const numMap = {}; 
+function twoSum(nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (complement in numMap) { // Or numMap.has(complement) for a Map
-            return [numMap[complement], i];
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
         }
-        numMap[nums[i]] = i;
     }
-};
+    return []; // if no solution found
+}
+
+// Default test case
+const nums = [2, 7, 11, 15];
+const target = 9;
+
+// Call and print result
+const result = twoSum(nums, target);
+console.log("Output:", result); // Expected: [0, 1]
