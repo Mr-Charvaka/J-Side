4418ddf76164ea415862f5c89a394cac14098b73
FlickshotFire <136790125+FlickshotFire@users.noreply.github.com>
2025-08-04 22:36:29 +0530

    feat: update Two Sum.js

---
+++ Two Sum.js
@@ -0,0 +1,9 @@
+const twoSum = function(nums, target) {
+    const numMap = {}; // Or new Map()
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (complement in numMap) { // Or numMap.has(complement) for a Map
+            return [numMap[complement], i];
+        }
+        numMap[nums[i]] = i;
+    }
