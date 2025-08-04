016222e316f03c84d63bcbaf272da1930b61679c
FlickshotFire <136790125+FlickshotFire@users.noreply.github.com>
2025-08-04 10:21:55 +0530

    feat: update new.js

--- new.js
+++ new.js
@@ -1,3 +1,2 @@
-    // First, install it: npm install readline-sync    const readlineSync = require('readline-sync');
     const a = readlineSync.question('What is your name? ');
     console.log(a);
