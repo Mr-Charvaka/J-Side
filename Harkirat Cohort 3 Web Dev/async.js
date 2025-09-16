a6d08cf4a294ed0c508f3c097154d3a5ddb04ad0
Mr-Charvaka <136790125+Mr-Charvaka@users.noreply.github.com>
2025-09-16 13:50:15 +0530

    Update async.js

--- Harkirat Cohort 3 Web Dev/async.js
+++ Harkirat Cohort 3 Web Dev/async.js
@@ -4,7 +4,8 @@ fs.readfile("a.txt","utf-8", func(err, data)){
 console.log(data);
 }
 console.log("Async");
-for (let i=0;i<1000000000000;i++){
+for (let i=0;i<1000000000000;i++)
+{
 a++;
 }
 console.log ("Async called");
